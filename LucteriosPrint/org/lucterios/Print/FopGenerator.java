5e19156dcf15aa4e9d2e2f261ec3d3fc1a534ccb
user <user@69e63612-969a-4371-9866-b14aae95a0cb>
2009-07-27 20:50:55 +0000

    Client 0.98 => Robot StressTest
    git-svn-id: http://projets.lucterios.org/svn/Clients/java@563 69e63612-969a-4371-9866-b14aae95a0cb

--- LucteriosPrint/org/lucterios/Print/FopGenerator.java
+++ LucteriosPrint/org/lucterios/Print/FopGenerator.java
@@ -26,7 +26,6 @@ import java.awt.print.*;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
